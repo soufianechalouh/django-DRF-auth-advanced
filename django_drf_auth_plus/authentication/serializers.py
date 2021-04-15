068b22b911720e5c79532f6aee20af43cea2d51b
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-15 18:11:10 +0000

    Serializer for registration

---
+++ django_drf_auth_plus/authentication/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["email", "username", "password"]
+
+    def validate(self, attrs):
+        return super().validate(attrs)
