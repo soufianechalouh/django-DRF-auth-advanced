c6ff9399db661ac3482270c3b18179f9188d5d1d
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-15 18:54:46 +0000

    Registration view

--- django_drf_auth_plus/authentication/serializers.py
+++ django_drf_auth_plus/authentication/serializers.py
@@ -10,4 +10,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ["email", "username", "password"]
 
     def validate(self, attrs):
+        username = attrs.get("username", "")
+
+        if not str(username).isalnum():
+            raise serializers.ValidationError("username should be alphanumeric")
         return super().validate(attrs)
+
+    def create(self, validated_data):
+        return User.object.create_user(**validated_data)
--- django_drf_auth_plus/authentication/views.py
+++ django_drf_auth_plus/authentication/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import RegisterSerializer
 
-# Create your views here.
+
+class RegisterView(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        user = self.serializer_class(data=request.data)
+        user.is_valid(raise_exception=True)
+        user.save()
+
+        user_data = user.data
+        return Response(user_data, status=status.HTTP_201_CREATED)
