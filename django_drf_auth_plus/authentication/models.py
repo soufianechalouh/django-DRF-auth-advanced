35e5b78030d232d5f07f43c588063c37a7225e6d
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-15 05:46:36 +0000

    Create User Manager

--- django_drf_auth_plus/authentication/models.py
+++ django_drf_auth_plus/authentication/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager"""
+
+    def create_user(self, username, email, password=None):
+
+        if username is None:
+            raise TypeError("User should have Username")
+
+        if email is None:
+            raise TypeError("User should have email")
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+
+        if password is None:
+            raise TypeError("User should have Password")
+
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
