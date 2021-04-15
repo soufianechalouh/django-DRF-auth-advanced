eb721bd2a5b61581929a3c1747ac337062251ccb
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-15 18:01:10 +0000

    Create User model

--- django_drf_auth_plus/authentication/models.py
+++ django_drf_auth_plus/authentication/models.py
@@ -32,3 +32,24 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.save()
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=255, unique=True, db_index=True)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    object = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        return ''
