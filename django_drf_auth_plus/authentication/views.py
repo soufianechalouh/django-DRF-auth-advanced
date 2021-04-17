cee9d1cb4af7107fc2acdd5c5d715cd7c9bde646
Soufiane CHALOUH <soufianechalouh@gmail.com>
2021-04-17 12:33:50 +0000

    Send verification email

--- django_drf_auth_plus/authentication/views.py
+++ django_drf_auth_plus/authentication/views.py
@@ -2,6 +2,11 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import RegisterSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import User
+from .utils import Util
+from django.contrib.sites.shortcuts import get_current_site
+from django.urls import reverse
 
 
 class RegisterView(GenericAPIView):
@@ -13,4 +18,21 @@ class RegisterView(GenericAPIView):
         user.save()
 
         user_data = user.data
+        user = User.object.get(email=user_data["email"])
+
+        token = RefreshToken.for_user(user).access_token
+
+        current_site = get_current_site(request).domain
+        relative_link = reverse("email-verify")
+        abs_url = "http://" + current_site + relative_link + "?token=" + str(token)
+        email_body = "Hi " + user.username + ", Use link bellow to verify your emai \n" + abs_url
+        data = {"email_body": email_body, "to": user.email, "email_subject": "Verify your account", "domain": current_site}
+
+        Util.send_email(data)
         return Response(user_data, status=status.HTTP_201_CREATED)
+
+
+class VerifyEmail(GenericAPIView):
+
+    def get(self):
+        pass
--- django_drf_auth_plus/django_drf_auth_plus/settings.py
+++ django_drf_auth_plus/django_drf_auth_plus/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -41,6 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_drf_auth_plus.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -82,7 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -102,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -116,7 +118,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -126,3 +127,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "whatever@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "******")
